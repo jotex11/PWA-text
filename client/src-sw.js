ced8622dd7d059853003ea689ab6ba491cdd84b6
joseph <jomill0723@outlook.com>
2023-11-11 12:40:13 -0500

    added todo to src-sw.js

--- client/src-sw.js
+++ client/src-sw.js
@@ -27,4 +27,17 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(
+  /\.(js|css|png|jpg|svg|gif)$/,
+  new CacheFirst({
+    cacheName: 'asset-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxAgeSeconds: 7 * 24 * 60 * 60,
+      })
+    ]
+  })
+);
