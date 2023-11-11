df633762ff4507fc0b3448173006a17bd8dac53c
joseph <jomill0723@outlook.com>
2023-11-11 12:21:12 -0500

    added first method

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,17 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
 
+export const putDb = async (content) => {
+  const db = await initdb();
+  const tx = db.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const id = await store.add(content);
+  console.log(`Added content with ID: ${id}`);
+  await tx.done;
+};
 // TODO: Add logic for a method that gets all the content from the database
+
 export const getDb = async () => console.error('getDb not implemented');
 
 initdb();
