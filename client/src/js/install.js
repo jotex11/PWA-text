013d45d65e49aad9fce16bba98c54930d364cbcb
joseph <jomill0723@outlook.com>
2023-11-11 12:56:37 -0500

    Added todos code in install.js

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,40 @@
 const butInstall = document.getElementById('buttonInstall');
 
+let deferredPrompt;
+
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+window.addEventListener('beforeinstallprompt', (event) => {
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+  event.preventDefault();
+  
+  deferredPrompt = event;
+  
+  // TODO: Show a custom install button or UI to prompt the user to install the app
+  butInstall.style.display = 'block';
+});
+
+butInstall.addEventListener('click', async () => {
+  // TODO: Show the install prompt when the custom install button is clicked
+  if (deferredPrompt) {
+    deferredPrompt.prompt();
+    
+    const userChoice = await deferredPrompt.userChoice;
+    
+    
+    if (userChoice.outcome === 'accepted') {
+      console.log('User accepted the install prompt');
+    } else {
+      console.log('User dismissed the install prompt');
+    }
+    
+    deferredPrompt = null;
+  }
+});
+
+window.addEventListener('appinstalled', (event) => {
+  // TODO: Perform actions when the app is successfully installed
+  console.log('App installed successfully');
+  
+  butInstall.style.display = 'none';
+});
